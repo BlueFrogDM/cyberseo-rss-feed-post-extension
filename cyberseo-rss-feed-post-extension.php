caa539215052403588e81618ae43b56e1c2c3e68
BlueFrogDM <40769295+BlueFrogDM@users.noreply.github.com>
2019-03-05 10:32:39 -0600

    Create cyberseo-rss-feed-post-extension.php

---
+++ cyberseo-rss-feed-post-extension.php
@@ -0,0 +1,124 @@
+<?php 
+
+/*
+ * Plugin Name: CyberSEO RSS Feed Post Extension Plugin
+ * Plugin URI: https://github.com/BlueFrogDM/cyberseo-rss-feed-post-extension/
+ * Description: Link Wordpress posts created from an RSS feed back to their original URL. Requires CyberSEO-created posts in order to properly function.
+ * Author: Blue Frog Marketing
+ * Author URI: https://www.bluefrogdm.com/
+ * Version: 1.0
+*/
+
+if( ! defined( 'ABSPATH' ) ) exit;
+
+class CyberSEO_post_link_to_original extends WP_Widget {
+
+/**
+ * Construct Widget
+ */
+    public function __construct() {
+        $widget_ops = array('classname' => 'post_link_original_widget_recent_entries', 'description' => __( "This widget returns recent posts created from an RSS feed and sets the post permalink to it's original URL." ) );
+        parent::__construct('recent-posts', __('Recent Posts from RSS Widget'), $widget_ops);
+        $this->alt_option_name = 'post_link_original_widget_recent_entries';
+
+        add_action( 'save_post', array($this, 'flush_widget_cache') );
+        add_action( 'deleted_post', array($this, 'flush_widget_cache') );
+        add_action( 'switch_theme', array($this, 'flush_widget_cache') );
+    }
+
+    public function widget($args, $instance) {
+        $cache = wp_cache_get('hubspot_widget_recent_posts', 'widget');
+
+        if ( !is_array($cache) )
+            $cache = array();
+
+        if ( ! isset( $args['widget_id'] ) )
+            $args['widget_id'] = $this->id;
+
+        if ( isset( $cache[ $args['widget_id'] ] ) ) {
+            echo $cache[ $args['widget_id'] ];
+            return;
+        }
+
+        ob_start();
+        extract($args);
+
+        $title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'Recent Posts' );
+        $title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
+        $number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 10;
+        if ( ! $number )
+            $number = 10;
+        $show_date = isset( $instance['show_date'] ) ? $instance['show_date'] : false;
+
+        $r = new WP_Query( apply_filters( 'widget_posts_args', array( 'posts_per_page' => $number, 'no_found_rows' => true, 'post_status' => 'publish', 'ignore_sticky_posts' => true ) ) );
+        if ($r->have_posts()) :
+?>
+        <?php echo $before_widget; ?>
+        <?php if ( $title ) echo $before_title . $title . $after_title; ?>
+        <ul>
+        <?php while ( $r->have_posts() ) : $r->the_post(); ?>
+        	<?php $hublink = get_post_custom_values('cyberseo_post_link'); ?>
+            <li>
+                <a href="<?php if ( $hublink ) echo $hublink[0]; else the_permalink(); ?>" title="<?php echo esc_attr( get_the_title() ? get_the_title() : get_the_ID() ); ?>"><?php if ( get_the_title() ) the_title(); else the_ID(); ?></a>
+            <?php if ( $show_date ) : ?>
+                <span class="post-date"><?php echo get_the_date(); ?></span>
+            <?php endif; ?>
+            </li>
+        <?php endwhile; ?>
+        </ul>
+        <?php echo $after_widget; ?>
+<?php
+        // Reset the global $the_post as this query will have stomped on it
+        wp_reset_postdata();
+
+        endif;
+
+        $cache[$args['widget_id']] = ob_get_flush();
+        wp_cache_set('hubspot_widget_recent_posts', $cache, 'widget');
+    }
+
+    public function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+        $instance['title'] = strip_tags($new_instance['title']);
+        $instance['number'] = (int) $new_instance['number'];
+        $instance['show_date'] = (bool) $new_instance['show_date'];
+        $this->flush_widget_cache();
+
+        $alloptions = wp_cache_get( 'alloptions', 'options' );
+        if ( isset($alloptions['hubspot_widget_recent_entries']) )
+            delete_option('hubspot_widget_recent_entries');
+
+        return $instance;
+    }
+
+    public function flush_widget_cache() {
+        wp_cache_delete('hubspot_widget_recent_posts', 'widget');
+    }
+
+    public function form( $instance ) {
+        $title     = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+        $number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
+        $show_date = isset( $instance['show_date'] ) ? (bool) $instance['show_date'] : false;
+?>
+        <p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
+
+        <p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
+        <input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+
+        <p><input class="checkbox" type="checkbox" <?php checked( $show_date ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" />
+        <label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Display post date?' ); ?></label></p>
+<?php
+    }
+}
+
+/*
+ * Register the Widget
+ */
+
+function post_link_original_widget() {
+
+    register_widget( 'CyberSEO_post_link_to_original' );
+}
+
+add_action( 'widgets_init', 'post_link_original_widget' );
